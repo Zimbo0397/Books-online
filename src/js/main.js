06ec79afe34fe2c76ebd12756fe6ad0cc3a753a6
Zimbo0397 <zimbo0397@Gmail.com>
2017-01-03 18:19:27 +0200

    -mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,46 @@ var sortHolder = $('#sort-holder'),
 		});
 	});
 
-	
+
+$('#mobile-nav-btn').on('click', function(e) {
+	e.preventDefault();
+	$('#nav').toggleClass('open');
+});
+
+$('#sort-holder').on('click', function(e) {
+	$(this).toggleClass('open');
+});
+
+$('#aside-open').on('click', function() {
+	$('#aside-panel').addClass('open');
+	$('.black-plate').addClass('active');
+})
+
+$('#aside-slose').on('click', function() {
+	$('#aside-panel').removeClass('open');
+	$('.black-plate').removeClass('active');
+});
+
+$('.black-plate').on('click', function() {
+	$('#aside-panel').removeClass('open');
+	$(this).removeClass('active');
+});
+
+$('#font-modal-open').on('click', function() {
+	$('#font-modal').addClass('open');
+	$('body').addClass('fixed');
+});
+
+$('#chapter-modal-open').on('click', function() {
+	$('#chapter-modal').addClass('open');
+	$('body').addClass('fixed');
+});
+
+$('#font-modal .close-btn').on('click', function() {
+	$('#font-modal').removeClass('open');
+	$('body').removeClass('fixed');
+});
+$('#chapter-modal .close-btn').on('click', function() {
+	$('#chapter-modal').removeClass('open');
+	$('body').removeClass('fixed');
+});
